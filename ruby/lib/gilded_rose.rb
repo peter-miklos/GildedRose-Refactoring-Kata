cefd97065ee38f37bf4ae44412009fc9833f5d5a
Peter Miklos <pt.miklos@gmail.com>
2016-10-19 15:08:05 +0100

    constants and some more refactoring added

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -4,8 +4,12 @@ class GildedRose
 
   METHOD_HELPER = {"Aged Brie" => "aged_brie",
                    "Backstage passes to a TAFKAL80ETC concert" => "b_pass"}
+  MAX_QUALITY = 50
+  MIN_QUALITY = 0
+  Q_CHANGE = 1
+  CHANGED_DAYS = 1
 
-  def initialize(items, method_helper = METHOD_HELPER)
+  def initialize(items, method_helper: METHOD_HELPER)
     @items = items
     @method_helper = method_helper
   end
@@ -13,36 +17,45 @@ class GildedRose
   def update_quality()
     @items.reject{|i| i.name == "Sulfuras, Hand of Ragnaros"}.each do |item|
       update_sell_in(item)
-      update_quality_of_an_item(item) if item.quality > 0 && item.quality < 50
+      update_quality_of_an_item(item) if quality_in_range?(item)
     end
   end
 
   private
 
   def update_sell_in(item)
-    item.sell_in -= 1
+    item.sell_in -= CHANGED_DAYS
   end
 
   def update_quality_of_an_item(item)
-    m_name = @method_helper[item.name] == nil ? "update_normal_items_quality" : "update_#{@method_helper[item.name]}_quality"
+    m_name = @method_helper[item.name] == nil ? "update_normal_items_quality" :
+             "update_#{@method_helper[item.name]}_quality"
     send(m_name.to_sym, item)
   end
 
   def update_aged_brie_quality(item)
-    item.quality += 1
-    item.quality += 1 if item.sell_in < 1 && item.quality < 50
+    item.quality += Q_CHANGE
+    item.quality += Q_CHANGE if double_the_quality_change?(item)
   end
 
   def update_b_pass_quality(item)
     if item.sell_in < 1 then item.quality -= item.quality
-    elsif item.sell_in < 6 then item.quality += 3
-    elsif item.sell_in < 11 then item.quality += 2
-    else item.quality += 1
+    elsif item.sell_in < 6 then item.quality += Q_CHANGE * 3
+    elsif item.sell_in < 11 then item.quality += Q_CHANGE * 2
+    else item.quality += Q_CHANGE
     end
   end
 
   def update_normal_items_quality(item)
-    item.quality -= 1
-    item.quality -= 1 if item.sell_in < 1 && item.quality > 0
+    item.quality -= Q_CHANGE
+    item.quality -= Q_CHANGE if double_the_quality_change?(item)
+  end
+
+  def double_the_quality_change?(item)
+    item.sell_in < 1 && quality_in_range?(item)
+  end
+
+  def quality_in_range?(item)
+    item.quality > MIN_QUALITY && item.quality < MAX_QUALITY
   end
 end
