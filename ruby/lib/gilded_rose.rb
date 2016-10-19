815a42a80ddba2cb0f2a2879c8ffccf3a6500a13
Peter Miklos <pt.miklos@gmail.com>
2016-10-19 14:41:48 +0100

    update_quality_of_an_item refactored

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -2,8 +2,12 @@ require 'item'
 
 class GildedRose
 
-  def initialize(items)
+  METHOD_HELPER = {"Aged Brie" => "aged_brie",
+                   "Backstage passes to a TAFKAL80ETC concert" => "b_pass"}
+
+  def initialize(items, method_helper = METHOD_HELPER)
     @items = items
+    @method_helper = method_helper
   end
 
   def update_quality()
@@ -12,36 +16,33 @@ class GildedRose
       update_quality_of_an_item(item) if item.quality > 0 && item.quality < 50
     end
   end
-end
 
-private
+  private
 
-def update_sell_in(item)
-  item.sell_in -= 1
-end
-
-def update_quality_of_an_item(item)
-  case item.name
-  when "Aged Brie" then update_aged_brie_quality(item)
-  when "Backstage passes to a TAFKAL80ETC concert" then update_b_pass_quality(item)
-  else update_non_reserved_items_quality(item)
+  def update_sell_in(item)
+    item.sell_in -= 1
   end
-end
 
-def update_aged_brie_quality(item)
-  item.quality += 1
-  item.quality += 1 if item.sell_in < 1 && item.quality < 50
-end
-
-def update_b_pass_quality(item)
-  if item.sell_in < 1 then item.quality -= item.quality
-  elsif item.sell_in < 6 then item.quality += 3
-  elsif item.sell_in < 11 then item.quality += 2
-  else item.quality += 1
+  def update_quality_of_an_item(item)
+    m_name = @method_helper[item.name] == nil ? "update_normal_items_quality" : "update_#{@method_helper[item.name]}_quality"
+    send(m_name.to_sym, item)
   end
-end
 
-def update_non_reserved_items_quality(item
-  item.quality -= 1
-  item.quality -= 1 if item.sell_in < 1 && item.quality > 0
+  def update_aged_brie_quality(item)
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 1 && item.quality < 50
+  end
+
+  def update_b_pass_quality(item)
+    if item.sell_in < 1 then item.quality -= item.quality
+    elsif item.sell_in < 6 then item.quality += 3
+    elsif item.sell_in < 11 then item.quality += 2
+    else item.quality += 1
+    end
+  end
+
+  def update_normal_items_quality(item)
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in < 1 && item.quality > 0
+  end
 end
