baeb54f80c2531810235ed1e92d3cb0b660b07ca
Peter Miklos <pt.miklos@gmail.com>
2016-10-19 14:19:29 +0100

    update_quality method refactored

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -7,26 +7,9 @@ class GildedRose
   end
 
   def update_quality()
-    @items.each do |item|
+    @items.reject{|i| i.name == "Sulfuras, Hand of Ragnaros"}.each do |item|
       update_sell_in(item)
-
-      if item.quality > 0 && item.quality < 50
-        case item.name
-        when "Aged Brie"
-          item.quality += 1
-          item.quality += 1 if item.sell_in < 1 && item.quality < 50
-        when "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in < 1 then item.quality -= item.quality
-          elsif item.sell_in < 6 then item.quality += 3
-          elsif item.sell_in < 11 then item.quality += 2
-          else item.quality += 1
-          end
-        when "Sulfuras, Hand of Ragnaros" #do nothing
-        else
-          item.quality -= 1
-          item.quality -= 1 if item.sell_in < 1 && item.quality > 0
-        end
-      end
+      update_quality_of_an_item(item) if item.quality > 0 && item.quality < 50
     end
   end
 end
@@ -34,5 +17,31 @@ end
 private
 
 def update_sell_in(item)
-  item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
+  item.sell_in -= 1
+end
+
+def update_quality_of_an_item(item)
+  case item.name
+  when "Aged Brie" then update_aged_brie_quality(item)
+  when "Backstage passes to a TAFKAL80ETC concert" then update_b_pass_quality(item)
+  else update_non_reserved_items_quality(item)
+  end
+end
+
+def update_aged_brie_quality(item)
+  item.quality += 1
+  item.quality += 1 if item.sell_in < 1 && item.quality < 50
+end
+
+def update_b_pass_quality(item)
+  if item.sell_in < 1 then item.quality -= item.quality
+  elsif item.sell_in < 6 then item.quality += 3
+  elsif item.sell_in < 11 then item.quality += 2
+  else item.quality += 1
+  end
+end
+
+def update_non_reserved_items_quality(item
+  item.quality -= 1
+  item.quality -= 1 if item.sell_in < 1 && item.quality > 0
 end
