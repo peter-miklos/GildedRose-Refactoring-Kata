cca2f8e3d8affedb232066bfa00a3d79cdc78928
Peter Miklos <pt.miklos@gmail.com>
2016-10-19 12:12:57 +0100

    unit tests updated

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -15,13 +15,13 @@ describe GildedRose do
 
     context "normal item's quality" do
 
-      it "lowers the value by one at EOD if sell_in value is not 0" do
+      it "lowers the value by one at EOD if sell by date has not passed" do
         items = [Item.new("test_item", 30, 30)]
         GildedRose.new(items).update_quality
         expect(items.first.quality).to eq 29
       end
 
-      it "lowers the value by two at EOD if sell_in value is 0 or less" do
+      it "lowers the value by two at EOD if sell by date has passed" do
         items = [Item.new("test_item", -1, 30)]
         GildedRose.new(items).update_quality
         expect(items.first.quality).to eq 28
@@ -32,20 +32,19 @@ describe GildedRose do
         GildedRose.new(items).update_quality
         expect(items.first.quality).to eq 0
       end
-
-      it "doesn't lower the value of the 'Sulfuras' at EOD" do
-        items = [Item.new("Sulfuras, Hand of Ragnaros", 30, 30)]
-        GildedRose.new(items).update_quality
-        expect(items.first.quality).to eq 30
-      end
-
     end
 
     context "Aged Brie's quality" do
-      it "increases the value by one at EOD" do
+      it "increases the value by one at EOD if sell by date has not passed" do
         items = [Item.new("Aged Brie", 30, 30)]
         GildedRose.new(items).update_quality
         expect(items.first.quality).to eq 31
+      end
+
+      it "increases the value by two at EOD if sell by date has passed" do
+        items = [Item.new("Aged Brie", 0, 30)]
+        GildedRose.new(items).update_quality
+        expect(items.first.quality).to eq 32
       end
 
       it "doesn't increase the value of the 'Aged Brie' if it's already 50" do
@@ -69,7 +68,7 @@ describe GildedRose do
       end
 
       it "increases the value by three at EOD if 5 days or less left" do
-        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 30)]
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 4, 30)]
         GildedRose.new(items).update_quality
         expect(items.first.quality).to eq 33
       end
@@ -81,8 +80,15 @@ describe GildedRose do
       end
     end
 
-    context "sell_in of all items" do
+    context "Sulfuras' quality" do
+      it "doesn't lower the value at EOD" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 30, 30)]
+        GildedRose.new(items).update_quality
+        expect(items.first.quality).to eq 30
+      end
+    end
 
+    context "sell_in of all items" do
       it "lowers the value by one at EOD" do
         items = [Item.new("test_item", 20, 30)]
         GildedRose.new(items).update_quality
@@ -94,7 +100,6 @@ describe GildedRose do
         GildedRose.new(items).update_quality
         expect(items.first.sell_in).to eq -1
       end
-
     end
 
   end
