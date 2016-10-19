5eb559da277b7a2b5ce26faa4c73a1348aede52b
Peter Miklos <pt.miklos@gmail.com>
2016-10-19 13:10:54 +0100

    logic moved to the case statement, all tests passed

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -13,19 +13,18 @@ class GildedRose
       if item.quality > 0 && item.quality < 50
         case item.name
         when "Aged Brie"
-          item.quality = item.quality + 1
+          item.quality += 1
+          item.quality += 1 if item.sell_in < 1 && item.quality < 50
         when "Backstage passes to a TAFKAL80ETC concert"
-          # item.quality = item.quality + 1
-          if item.sell_in < 6
-            item.quality = item.quality + 3
-          elsif item.sell_in < 11
-            item.quality = item.quality + 2
-          else
-            item.quality += 1
+          if item.sell_in < 1 then item.quality -= item.quality
+          elsif item.sell_in < 6 then item.quality += 3
+          elsif item.sell_in < 11 then item.quality += 2
+          else item.quality += 1
           end
         when "Sulfuras, Hand of Ragnaros" #do nothing
         else
           item.quality -= 1
+          item.quality -= 1 if item.sell_in < 1 && item.quality > 0
         end
       end
 
@@ -54,23 +53,23 @@ class GildedRose
       #   end
       # end
 
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      # if item.sell_in < 0
+        # if item.name != "Aged Brie"
+          # if item.name != "Backstage passes to a TAFKAL80ETC concert"
+            # if item.quality > 0
+            #   if item.name != "Sulfuras, Hand of Ragnaros"
+            #     item.quality = item.quality - 1
+            #   end
+            # end
+          # else
+            # item.quality = item.quality - item.quality
+          # end
+        # else
+          # if item.quality < 50
+          #   item.quality = item.quality + 1
+          # end
+        # end
+      # end
     end
   end
 end
